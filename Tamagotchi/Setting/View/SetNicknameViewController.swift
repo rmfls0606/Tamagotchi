ec78b5cbc5e18bb1287334c51c65fb7a65cce8c4
rmfls0606 <rmfls0606@naver.com>
2025-08-25 06:00:55 +0900

    feat: 뷰모델 기반 닉네임 저장 후 이전 화면으로 전환하는 바인딩 로직 작성

--- Tamagotchi/Setting/View/SetNicknameViewController.swift
+++ Tamagotchi/Setting/View/SetNicknameViewController.swift
@@ -12,6 +12,11 @@ import RxCocoa
 
 final class SetNicknameViewController: BaseViewController {
     
+    //MARK: -  Property
+    private let viewModel = SetNicknameViewModel()
+    private let disposeBag = DisposeBag()
+    
+    //MARK: - View
     private let nicknameTextField: UITextField = {
         let textField = UITextField()
         textField.text = UserManager.shared.currentUser?.nickName ?? "대장님"
@@ -25,11 +30,18 @@ final class SetNicknameViewController: BaseViewController {
         return view
     }()
     
+    private let nicknameValidLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12)
+        return label
+    }()
+    
     override func configureHierarchy() {
         
         nicknameTextField.addSubview(lineView)
         
         view.addSubview(nicknameTextField)
+        view.addSubview(nicknameValidLabel)
     }
     
     override func configureLayout() {
@@ -43,11 +55,53 @@ final class SetNicknameViewController: BaseViewController {
             make.bottom.horizontalEdges.equalToSuperview()
             make.height.equalTo(1)
         }
+        
+        nicknameValidLabel.snp.makeConstraints { make in
+            make.top.equalTo(nicknameTextField.snp.bottom).offset(8)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+        }
     }
     
     override func configureView() {
         view.backgroundColor = .white
         
         navigationItem.title = "\(UserManager.shared.currentUser?.nickName ?? "대장님") 이름 정하기"
+        let rightBarButtonItem = UIBarButtonItem(title: "저장", style: .plain, target: self, action: nil)
+        navigationItem.rightBarButtonItem = rightBarButtonItem
+    }
+    
+    override func configureBind() {
+        let input = SetNicknameViewModel.Input(
+            nickname: nicknameTextField.rx.text.orEmpty,
+            saveButtonTap: navigationItem.rightBarButtonItem?.rx.tap
+        )
+        
+        let output = viewModel.transform(input: input)
+        
+        output.nicknameValid
+            .bind(with: self) { owner, valid in
+                if valid{
+                    owner.nicknameValidLabel.text = "사용할 수 있는 닉네임입니다."
+                    owner.nicknameValidLabel.textColor = .green
+                }else{
+                    owner.nicknameValidLabel.text = "2글자 이상 6글자 이하로 입력해주세요.."
+                    owner.nicknameValidLabel.textColor = .red
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        output.submitEnabled
+            .bind(with: self, onNext: { owner, isEnabled in
+                if let button = owner.navigationItem.rightBarButtonItem{
+                    button.isEnabled = isEnabled
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        output.saveNickname
+            .bind(with: self) { owner, _ in
+                owner.navigationController?.popViewController(animated: true)
+            }
+            .disposed(by: disposeBag)
     }
 }
