f177433167e8b7c7390cbd4eb0da5d23c73b2700
rmfls0606 <rmfls0606@naver.com>
2025-08-25 04:51:12 +0900

    feat: 닉네임 설정 화면 UI 구성 및 AutoLayout 설정

--- Tamagotchi/Setting/View/SetNicknameViewController.swift
+++ Tamagotchi/Setting/View/SetNicknameViewController.swift
@@ -11,6 +11,40 @@ import RxSwift
 import RxCocoa
 
 final class SetNicknameViewController: BaseViewController {
+    
+    private let nicknameTextField: UITextField = {
+        let textField = UITextField()
+        textField.text = UserManager.shared.currentUser?.nickName ?? "대장님"
+        textField.font = .systemFont(ofSize: 14)
+        return textField
+    }()
+    
+    private let lineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.systemGray2
+        return view
+    }()
+    
+    override func configureHierarchy() {
+        
+        nicknameTextField.addSubview(lineView)
+        
+        view.addSubview(nicknameTextField)
+    }
+    
+    override func configureLayout() {
+        nicknameTextField.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.height.equalTo(44)
+        }
+        
+        lineView.snp.makeConstraints { make in
+            make.bottom.horizontalEdges.equalToSuperview()
+            make.height.equalTo(1)
+        }
+    }
+    
     override func configureView() {
         view.backgroundColor = .white
         
