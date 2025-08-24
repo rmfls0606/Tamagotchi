4f9b7b09b727c7f8a4d700b2d371d0c5eb6dad58
rmfls0606 <rmfls0606@naver.com>
2025-08-25 04:18:23 +0900

    feat: 설정 화면 테이블뷰를 만드는 데이터 모델 정의

---
+++ Tamagotchi/Setting/Model/Setting.swift
@@ -0,0 +1,14 @@
+//
+//  Setting.swift
+//  Tamagotchi
+//
+//  Created by 이상민 on 8/25/25.
+//
+
+import Foundation
+
+struct Setting {
+    let imageName: String
+    let title: String
+    let detail: String?
+}
