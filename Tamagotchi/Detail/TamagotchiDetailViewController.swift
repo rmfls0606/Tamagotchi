9c2fbb819d45ff0cb83dc129d65804dfdfa62846
rmfls0606 <rmfls0606@naver.com>
2025-08-24 20:56:30 +0900

    feat: 다마고치 상세 팝업 화면 UI 구성 및 AutoLayout 설정

---
+++ Tamagotchi/Detail/TamagotchiDetailViewController.swift
@@ -0,0 +1,168 @@
+//
+//  TamagotchiDetailViewController.swift
+//  Tamagotchi
+//
+//  Created by 이상민 on 8/24/25.
+//
+
+import UIKit
+import SnapKit
+
+final class TamagotchiDetailViewController: BaseViewController {
+    
+    //MARK: - View
+    private let MainBoxView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 10
+        return view
+    }()
+    
+    private let tamagotchiImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(named: "noImage")
+        return imageView
+    }()
+    
+    private let tamagotchiNameLabelBox: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 5
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.black.cgColor
+        return view
+    }()
+    
+    private(set) var tamagotchiNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .black
+        label.text = "준비중이에요"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let lineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray2
+        return view
+    }()
+    
+    private let introductionTextView: UITextView = {
+        let view = UITextView()
+        view.isScrollEnabled = false
+        view.text = "준비중입니다."
+        view.textAlignment = .center
+        return view
+    }()
+    
+    private let buttonLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray4
+        return view
+    }()
+    
+    private let buttonStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    private let cancelButton: UIButton = {
+        let button = UIButton()
+        var config = UIButton.Configuration.plain()
+        config.attributedTitle = AttributedString("취소", attributes: AttributeContainer([
+            .font: UIFont.systemFont(ofSize: 14)
+        ]))
+        config.baseForegroundColor = .black
+        config.contentInsets = NSDirectionalEdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16)
+        
+        button.backgroundColor = .systemGray6
+        button.configuration = config
+        return button
+    }()
+    
+    private let startButton: UIButton = {
+        let button = UIButton()
+        var config = UIButton.Configuration.plain()
+        config.attributedTitle = AttributedString("시작 ", attributes: AttributeContainer([
+            .font: UIFont.systemFont(ofSize: 14)
+        ]))
+        config.baseForegroundColor = .black
+        config.contentInsets = NSDirectionalEdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16)
+        
+        button.configuration = config
+        return button
+    }()
+    
+    override func configureHierarchy() {
+        MainBoxView.addSubview(tamagotchiImageView)
+        
+        tamagotchiNameLabelBox.addSubview(tamagotchiNameLabel)
+        MainBoxView.addSubview(tamagotchiNameLabelBox)
+        
+        MainBoxView.addSubview(lineView)
+        
+        MainBoxView.addSubview(introductionTextView)
+        
+        MainBoxView.addSubview(buttonLineView)
+        
+        buttonStackView.addArrangedSubview(cancelButton)
+        buttonStackView.addArrangedSubview(startButton)
+        
+        MainBoxView.addSubview(buttonStackView)
+        
+        view.addSubview(MainBoxView)
+    }
+    
+    override func configureLayout() {
+        MainBoxView.snp.makeConstraints { make in
+            make.width.equalTo(UIScreen.main.bounds.width * 0.8)
+            make.center.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        tamagotchiImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(50)
+            make.centerX.equalToSuperview()
+            make.size.equalTo(MainBoxView.snp.width).multipliedBy(0.4)
+        }
+        
+        tamagotchiNameLabelBox.snp.makeConstraints { make in
+            make.top.equalTo(tamagotchiImageView.snp.bottom).offset(8)
+            make.centerX.equalToSuperview()
+        }
+        
+        tamagotchiNameLabel.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(5)
+        }
+        
+        lineView.snp.makeConstraints { make in
+            make.height.equalTo(1)
+            make.horizontalEdges.equalToSuperview().inset(40)
+            make.top.equalTo(tamagotchiNameLabelBox.snp.bottom).offset(24)
+            
+        }
+        
+        introductionTextView.snp.makeConstraints { make in
+            make.top.equalTo(lineView.snp.bottom).offset(24)
+            make.horizontalEdges.equalToSuperview().inset(40)
+        }
+        
+        buttonLineView.snp.makeConstraints { make in
+            make.top.equalTo(introductionTextView.snp.bottom).offset(24)
+            make.height.equalTo(1)
+            make.horizontalEdges.equalToSuperview()
+        }
+        
+        buttonStackView.snp.makeConstraints { make in
+            make.top.equalTo(buttonLineView.snp.bottom)
+            make.horizontalEdges.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+    }
+    
+    override func configureView() {
+        view.backgroundColor = .darkGray.withAlphaComponent(0.8)
+    }
+}
