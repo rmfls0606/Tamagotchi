a5957b23bae173b760332e2859c72f8dd145a97b
rmfls0606 <rmfls0606@naver.com>
2025-08-25 04:20:40 +0900

    feat: 뷰모델 테이블뷰 데이터 기반 테이블 뷰 셀 생성

---
+++ Tamagotchi/Setting/View/SettingViewController.swift
@@ -0,0 +1,68 @@
+//
+//  SettingViewController.swift
+//  Tamagotchi
+//
+//  Created by 이상민 on 8/25/25.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class SettingViewController: BaseViewController {
+    
+    //MARK: - Property
+    private let viewModel = SettingViewModel()
+    private let disposeBag = DisposeBag()
+    
+    //MARK: - View
+    private let tableView: UITableView = {
+        let view = UITableView()
+        view.rowHeight = 50
+        return view
+    }()
+    
+    override func configureHierarchy() {
+        view.addSubview(tableView)
+    }
+    
+    override func configureLayout() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    override func configureView() {
+        view.backgroundColor = .white
+        
+        navigationItem.title = "설정"
+    }
+    
+    override func configureBind() {
+        let input = SettingViewModel.Input()
+        
+        let output = viewModel.transform(input: input)
+        
+        output.settingCellData
+            .bind(to: tableView.rx.items){ tableView, row, element in
+                
+                let cell = tableView.dequeueReusableCell(withIdentifier: "SettingTableViewCell") ?? UITableViewCell(style: .value1, reuseIdentifier: "SettingTableViewCell")
+                
+                cell.imageView?.image = UIImage(systemName: element.imageName)
+                cell.imageView?.tintColor = .black
+
+                cell.textLabel?.text = element.title
+                cell.textLabel?.font = .systemFont(ofSize: 14)
+                
+                cell.detailTextLabel?.text = element.detail
+                
+                cell.accessoryType = .disclosureIndicator
+                cell.separatorInset = .zero
+                cell.selectionStyle = .none
+                
+              return cell
+            }
+            .disposed(by: disposeBag)
+    }
+}
