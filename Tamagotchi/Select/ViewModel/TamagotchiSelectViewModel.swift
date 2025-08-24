2adccc97eba0e66195a55131817e4a78b2539ab3
rmfls0606 <rmfls0606@naver.com>
2025-08-24 17:24:23 +0900

    feat: RxSwift 기반 IIn-Output 패턴 TamagotchiSelectViewModel 생성

---
+++ Tamagotchi/Select/ViewModel/TamagotchiSelectViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  TamagotchiSelectViewModel.swift
+//  Tamagotchi
+//
+//  Created by 이상민 on 8/24/25.
+//
+
+import Foundation
+import RxSwift
+
+final class TamagotchiSelectViewModel {
+    
+    struct Input{
+        
+    }
+    
+    struct Output{
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}
