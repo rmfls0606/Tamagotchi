26731f745fe32d05df3b7e8c762176790d4c496d
rmfls0606 <rmfls0606@naver.com>
2025-08-24 19:21:45 +0900

    feat: 뷰모델의 다마고치 데이터를 기반으로 컬렉션뷰 셀 UI 구성

--- Tamagotchi/Select/View/TamagotchiSelectViewController.swift
+++ Tamagotchi/Select/View/TamagotchiSelectViewController.swift
@@ -7,22 +7,27 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 final class TamagotchiSelectViewController: BaseViewController {
     
+    //MARK: - Property
+    private let viewModel = TamagotchiSelectViewModel()
+    private let disposeBag = DisposeBag()
+    
+    //MARK: - View
     private let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         
         let width = UIScreen.main.bounds.width
         let cellCount = 3.0
-        let spacig = 16.0
+        let spacing = 16.0
         let padding = 16.0
         
-        let dimension = ((width - (padding * 2)) - (spacig * (cellCount - 1))) / cellCount
+        let dimension = ((width - (padding * 2)) - (spacing * (cellCount - 1))) / cellCount
         
         layout.estimatedItemSize = CGSize(width: dimension, height: dimension)
-        layout.minimumLineSpacing = spacig
-        layout.minimumInteritemSpacing = spacig
         layout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
         
         let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
@@ -44,5 +49,17 @@ final class TamagotchiSelectViewController: BaseViewController {
         view.backgroundColor = .white
         
         navigationItem.title = "다마고치 선택하기"
+    }
+    
+    override func configureBind() {
+        let input = TamagotchiSelectViewModel.Input()
+        
+        let output = viewModel.transform(input: input)
+        
+        output.tamagotchis
+            .bind(to: collectionView.rx.items(cellIdentifier: UICollectionViewCell.identifier, cellType: UICollectionViewCell.self)){ item, element, cell in
+                cell.backgroundColor = .gray
+            }
+            .disposed(by: disposeBag)
     }
 }
