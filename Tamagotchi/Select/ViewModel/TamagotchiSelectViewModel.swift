f9a0f4c2dffd4f20b1428ed75e82df6c505e61ea
rmfls0606 <rmfls0606@naver.com>
2025-08-24 21:47:51 +0900

    feat: TamagotchiSelectViewModel에 선택 이벤트 스트림 추가

--- Tamagotchi/Select/ViewModel/TamagotchiSelectViewModel.swift
+++ Tamagotchi/Select/ViewModel/TamagotchiSelectViewModel.swift
@@ -11,19 +11,33 @@ import RxCocoa
 
 final class TamagotchiSelectViewModel {
     
+    private let disposeBag = DisposeBag()
+    
     struct Input{
-        
+        let cellSelected: ControlEvent<Tamagotchi>
     }
     
     struct Output{
         var tamagotchis: BehaviorRelay<[Tamagotchi]>
+        var selectedTamagotchi: PublishRelay<Tamagotchi>
     }
     
     func transform(input: Input) -> Output {
         let tamagotchis = BehaviorRelay<[Tamagotchi]>(value: [])
+        let tamagotchi = PublishRelay<Tamagotchi>()
         
         tamagotchis.accept(TamagotchiData.tamagotchis)
         
-        return Output(tamagotchis: tamagotchis)
+        input.cellSelected
+            .asDriver()
+            .drive { tamagotch in
+                tamagotchi.accept(tamagotch)
+            }
+            .disposed(by: disposeBag)
+        
+        return Output(
+            tamagotchis: tamagotchis,
+            selectedTamagotchi: tamagotchi
+        )
     }
 }
