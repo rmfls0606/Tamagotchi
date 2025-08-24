31dcf0b43c44d5c62642b76a61e1c2ae7b1ea7a8
rmfls0606 <rmfls0606@naver.com>
2025-08-24 16:46:17 +0900

    Initial Commit

---
+++ Tamagotchi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tamagotchi
+//
+//  Created by 이상민 on 8/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
