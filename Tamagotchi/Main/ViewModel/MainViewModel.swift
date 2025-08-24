239be4584f56fe6d6f14ac5d7b55831e48ffe845
rmfls0606 <rmfls0606@naver.com>
2025-08-25 02:22:30 +0900

    feat: RxSwift 기반 In-Out패턴 MainViewModel 생성 및 밥먹기 버튼 클릭 바인딩 로직 작성

---
+++ Tamagotchi/Main/ViewModel/MainViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  MainViewModel.swift
+//  Tamagotchi
+//
+//  Created by 이상민 on 8/25/25.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class MainViewModel {
+    
+    private let disposeBag = DisposeBag()
+    
+    struct Input{
+        let riceEatButtonTapped: ControlEvent<Void>
+        let riceValue: ControlProperty<String>
+    }
+    
+    struct Output{
+        let user: PublishRelay<User>
+    }
+    
+    func transform(input: Input) -> Output {
+        let userRelay = PublishRelay<User>()
+        
+        input.riceEatButtonTapped
+            .withLatestFrom(input.riceValue)
+            .map { value in
+                guard let value = Int(value), !String(value).isEmpty else { return 1}
+                
+                if value >= 1 && value <= 99{
+                    return value
+                }
+                return 0
+            }
+            .bind(with: self) { owner, value in
+                if let currntUser = UserManager.shared.currentUser{
+                    let newRiceCount = currntUser.riceCount + value
+                    let level = owner.calculateLevel(rice: newRiceCount, water: currntUser.waterCount)
+                    let imageNumber = min(level, 9)
+                    
+                    var newUser = currntUser
+                    newUser.imageName = "\(currntUser.imageName.prefix(1))-\(imageNumber)"
+                    newUser.riceCount = newRiceCount
+                    newUser.level = level
+                    UserManager.shared.updateUser(newUser: newUser)
+                    userRelay.accept(newUser)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        return Output(user: userRelay)
+    }
+    
+    private func calculateLevel(rice: Int, water: Int) -> Int {
+        let value = max(1, ((rice / 5) + (water / 2)) / 10)
+        return min(value, 10)
+    }
+}
