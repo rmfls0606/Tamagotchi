75ecb5664596bd29a715b1e0e6752f03004d2098
rmfls0606 <rmfls0606@naver.com>
2025-08-25 20:47:43 +0900

    feat: Lotto 화면 파일 생성

---
+++ Tamagotchi/Lotto/LottoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LottoViewController.swift
+//  Tamagotchi
+//
+//  Created by 이상민 on 8/25/25.
+//
+
+import UIKit
+
+final class LottoViewController: BaseViewController {
+
+    override func configureView() {
+        view.backgroundColor = .white
+        
+        navigationItem.title = "Lotto"
+    }
+}
