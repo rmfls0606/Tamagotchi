092ffc649cda507f383737da8f384b70fd9e11d9
rmfls0606 <rmfls0606@naver.com>
2025-08-26 03:45:03 +0900

    feat: 하나의 탭에서만 rootVC 전환하기 위한 Notification.name 생성

---
+++ Tamagotchi/Extension/Notification+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Notification+Extension.swift
+//  Tamagotchi
+//
+//  Created by 이상민 on 8/26/25.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let tamagotchiTabViewChange = Notification.Name("tamagotchiTabViewChange")
+}
