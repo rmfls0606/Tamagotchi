c229b461efbea2028d8638367cd960db1bf367eb
rmfls0606 <rmfls0606@naver.com>
2025-08-25 23:46:09 +0900

    feat: BoxOffice 화면 UI 구성 및 AutoLayout 설정

--- Tamagotchi/BoxOffice/BoxOfficeViewController.swift
+++ Tamagotchi/BoxOffice/BoxOfficeViewController.swift
@@ -6,8 +6,42 @@
 //
 
 import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
 
 final class BoxOfficeViewController: BaseViewController {
+    
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.searchTextField.backgroundColor = .systemGray6
+        searchBar.placeholder = "yyyyMMdd"
+        return searchBar
+    }()
+    
+    private let tableView: UITableView = {
+        let view = UITableView()
+        view.rowHeight = 50
+        view.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return view
+    }()
+    
+    override func configureHierarchy() {
+        view.addSubview(searchBar)
+        view.addSubview(tableView)
+    }
+    
+    override func configureLayout() {
+        searchBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom)
+            make.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
 
     override func configureView() {
         view.backgroundColor = .white
