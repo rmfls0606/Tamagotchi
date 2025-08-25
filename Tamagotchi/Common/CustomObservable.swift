ca4912f076767eadd68bcdae17a4c8eca53c8157
rmfls0606 <rmfls0606@naver.com>
2025-08-26 02:18:13 +0900

    feat: BoxOffice API 요청을 Observable로 CustomObservable 구현

--- Tamagotchi/Common/CustomObservable.swift
+++ Tamagotchi/Common/CustomObservable.swift
@@ -15,7 +15,22 @@ final class CustomObservable{
         
         return Observable<Lotto>.create { observer in
             
-            NetworkManager.shared.callRequest(api: .main(drwNo: draNo), type: Lotto.self) { response in
+            NetworkManager.shared.callRequest(api: LottoRouter.main(drwNo: draNo), type: Lotto.self) { response in
+                observer.onNext(response)
+                observer.onCompleted()
+            } failureHandler: { error in
+                observer.onError(error)
+            }
+
+            return Disposables.create()
+        }
+    }
+    
+    static func getBoxOffice(targetDt: String) -> Observable<BoxOffice>{
+        
+        return Observable<BoxOffice>.create { observer in
+            
+            NetworkManager.shared.callRequest(api: BoxOfficeRouter.main(targetDt: targetDt), type: BoxOffice.self) { response in
                 observer.onNext(response)
                 observer.onCompleted()
             } failureHandler: { error in
