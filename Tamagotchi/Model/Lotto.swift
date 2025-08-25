2483b2da9fa68d2bc5c21a5604d45ed190c469a3
rmfls0606 <rmfls0606@naver.com>
2025-08-25 22:26:31 +0900

    feat: Lotto API 응답 모델 정의

---
+++ Tamagotchi/Model/Lotto.swift
@@ -0,0 +1,17 @@
+//
+//  Lotto.swift
+//  Tamagotchi
+//
+//  Created by 이상민 on 8/25/25.
+//
+
+import Foundation
+
+struct Lotto: Decodable{
+    let drwtNo1: Int
+    let drwtNo2: Int
+    let drwtNo3: Int
+    let drwtNo4: Int
+    let drwtNo5: Int
+    let drwtNo6: Int
+}
