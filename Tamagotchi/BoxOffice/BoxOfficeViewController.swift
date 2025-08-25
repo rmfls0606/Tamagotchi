dabd9ceacfd9902d17bee46f2201072e479f551a
rmfls0606 <rmfls0606@naver.com>
2025-08-25 20:47:57 +0900

    feat: BoxOffice 화면 파일 생성

---
+++ Tamagotchi/BoxOffice/BoxOfficeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BoxOfficeViewController.swift
+//  Tamagotchi
+//
+//  Created by 이상민 on 8/25/25.
+//
+
+import UIKit
+
+final class BoxOfficeViewController: BaseViewController {
+
+    override func configureView() {
+        view.backgroundColor = .white
+        
+        navigationItem.title = "Box Office"
+    }
+}
