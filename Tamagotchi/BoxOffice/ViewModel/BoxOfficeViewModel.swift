84280ea97845419ccd401cf3bc50e9e50d17924e
rmfls0606 <rmfls0606@naver.com>
2025-08-26 21:17:43 +0900

    feat: BaseViewModel 기반 BoxOfficeViewModel 파일 생성

---
+++ Tamagotchi/BoxOffice/ViewModel/BoxOfficeViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  BoxOfficeViewModel.swift
+//  Tamagotchi
+//
+//  Created by 이상민 on 8/26/25.
+//
+
+import Foundation
+
+final class BoxOfficeViewModel: BaseViewModel{
+    
+    struct Input{
+        
+    }
+    
+    struct Output{
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}
